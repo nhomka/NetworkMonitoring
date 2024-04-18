9c3059934d5cfe17af53f981b25124f40301392d
Nathan <nathan.homka@gmail.com>
2024-04-17 23:19:25 -0700

    refactor

--- datetime_util.py
+++ datetime_util.py
@@ -9,10 +9,10 @@ def date_to_remove_old_files(days_to_subtract: int) -> datetime.date:
     return datetime.now().date() - timedelta(days=days_to_subtract)
 
 #test-validated
-def is_start_of_day(delay_in_seconds: int) -> bool:
+def is_start_of_day(delay_secs: int) -> bool:
     current_time = datetime.now()
     return \
-        current_time <= datetime.now().replace(hour=0, minute=(delay_in_seconds//60), second = delay_in_seconds % 60) \
+        current_time <= datetime.now().replace(hour=0, minute=(delay_secs//60), second = delay_secs % 60) \
         and current_time >= datetime.now().replace(hour=0, minute=0, second = 0)
         
 #test-validated
